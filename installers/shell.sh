d999c77741a45101a3c69f306aae25c93eb0ff47
Bruno Gaspar <brunofgaspar1@gmail.com>
2018-10-15 00:23:38 +0100

    chore: Update shell installer
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- installers/shell.sh
+++ installers/shell.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+# Add shells installed by Homebrew
+sudo bash -c "which zsh bash >> /etc/shells"
+
 # Make ZSH the default shell environment
 chsh -s $(which zsh)
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
