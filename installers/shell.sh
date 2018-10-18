cb46e825a1c211c7098ba3490a5badc61d2efdb5
Bruno Gaspar <brunofgaspar1@gmail.com>
2018-10-18 20:18:36 +0100

    chore: Update shell installer
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- installers/shell.sh
+++ installers/shell.sh
@@ -6,6 +6,9 @@ sudo bash -c "which zsh bash >> /etc/shells"
 # Make ZSH the default shell environment
 chsh -s $(which zsh)
 
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
