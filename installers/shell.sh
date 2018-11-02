a07a2e231217a2281220c1084b8d3ff615d398ff
Bruno Gaspar <brunofgaspar1@gmail.com>
2018-11-02 02:26:22 +0000

    chore: Tweak shell installer
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- installers/shell.sh
+++ installers/shell.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Hide "last login" line when starting a new terminal session
+touch $HOME/.hushlogin
+
 # Add shells installed by Homebrew
 sudo bash -c "which zsh bash >> /etc/shells"
 
@@ -7,6 +10,7 @@ sudo bash -c "which zsh bash >> /etc/shells"
 chsh -s $(which zsh)
 
 # Install oh-my-zsh
+rm -rf $HOME/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
