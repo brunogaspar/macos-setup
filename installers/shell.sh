a86f8a4567e19ade1ac8c6392347bdab9bd4ab04
Bruno Gaspar <brunofgaspar1@gmail.com>
2018-11-03 17:59:52 +0000

    fix: Shell installer
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- installers/shell.sh
+++ installers/shell.sh
@@ -9,10 +9,6 @@ sudo bash -c "which zsh bash >> /etc/shells"
 # Make ZSH the default shell environment
 chsh -s $(which zsh)
 
-# Install oh-my-zsh
-rm -rf $HOME/.oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
@@ -21,3 +17,7 @@ ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 rm -f $HOME/.gitignore $HOME/.gitconfig
 ln -s $HOME/.dotfiles/git/gitignore $HOME/.gitignore
 ln -s $HOME/.dotfiles/git/gitconfig $HOME/.gitconfig
+
+# Install oh-my-zsh
+rm -rf $HOME/.oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
