be46cdbe61e23802463df1af7f512c3ea5af8d7a
Bruno Gaspar <brunofgaspar1@gmail.com>
2018-11-04 19:23:50 +0000

    chore: Update development installer
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- installers/development.sh
+++ installers/development.sh
@@ -2,6 +2,10 @@
 
 # Install PHP extensions with PECL
 pecl install imagick
+pecl install mongodb
+pecl install redis
+pecl install xdebug
+pecl install yaml
 
 # Install Laravel Valet
 $HOME/.composer/vendor/bin/valet install
